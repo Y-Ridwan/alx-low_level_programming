a7ba65f44225d02f530986eae6f66fb1114cea52
root <yrkbolt@gmail.com>
2023-12-17 04:55:16 -0800

    if statement to test whether a variable number value is positive or negative as is it greater or lesser than zero.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * return: always zero
+ */
+
+
 int main(void)
 {
 	int n;
@@ -13,15 +19,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d, is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	if (n == 0)
 	{
-		printf("%d, is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	if (n < 0)
 	{
-		printf("%d, is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
